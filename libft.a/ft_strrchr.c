12794589e73c172ad39d702a825480c3bdd319d5
sarroyo- <me@example.com>
2024-04-26 17:43:22 +0200

    strchr's

--- libft.a/ft_strrchr.c
+++ libft.a/ft_strrchr.c
@@ -1,37 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sarroyo- <sarroyo-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/26 16:52:42 by sarroyo-          #+#    #+#             */
-/*   Updated: 2024/04/26 16:52:45 by sarroyo-         ###   ########.fr       */
+/*   Created: 2024/04/26 17:28:30 by sarroyo-          #+#    #+#             */
+/*   Updated: 2024/04/26 17:28:32 by sarroyo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <string.h>
 #include <stdio.h>
 
-char *ft_strchr (const char *s, int c)
+char *ft_strrchr(char *s, int c)
 {
     int i;
+    int p;
+
+    char* result;
 
     i = 0;
     while (s[i] != '\0')
     {
         if (s[i] == c)
-        {
-            return(s + i);
-        }
+            p = i;
         i++;
     }
-    return(NULL);
+    if (p)
+        return(s + i);
+    else
+        return(NULL);
 }
-/*
+
 int main ()
 {
-    char test [] = "Esto es un 7est.";
-    printf("Con ft_strchr: %s \n", ft_strchr(test, '.'));
-    printf("Con strchr: %s \n", strchr(test, '.'));
+    char test [] = "Esto es un tester.";
+    printf("Con ft_strrchr: %s \n", ft_strrchr(test, 'e'));
+    printf("Con strrchr: %s \n", strrchr(test, 'e'));
 }
-*/
